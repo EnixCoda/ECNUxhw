3145f5771cb662c5746158bd343c3a6874311dd6
ExinCoda <exincoda@gmail.com>
2016-12-15 21:54:23 +0800

    code style

--- tonight.php
+++ tonight.php
@@ -1,10 +1,10 @@
 <?php
 $reservations = json_decode(file_get_contents('reservations'), true);
 
-$twoDaysLater = date("Ymd", time() + 2 * 24 * 60 * 60);
-$fourDaysLater = date("Ymd", time() + 4 * 24 * 60 * 60);
+$twoDaysLater = date('Ymd', time() + 2 * 24 * 60 * 60);
+$fourDaysLater = date('Ymd', time() + 4 * 24 * 60 * 60);
 
-$push = array();
+$push = [];
 if (isset($reservations[$twoDaysLater])) {
 	$push[$twoDaysLater] = $reservations[$twoDaysLater];
 }
